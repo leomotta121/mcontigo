f057a30a45622a1047b357390f0b0e6fcb06aac3
Leonardo Motta <mail@leomotta.me>
2019-10-25 22:15:12 -0400

    add pagination

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import { useRouter } from 'next/router';
+
 import api from '~/services/api';
 
 import PostCard from '~/components/PostCard';
@@ -6,7 +9,9 @@ import PostCard from '~/components/PostCard';
 import { Container } from './styles';
 
 export default function Home({ search, tags, page }) {
+  const Router = useRouter();
   const [posts, setPosts] = useState([]);
+  const [maxPage, setMaxPage] = useState(0);
 
   useEffect(() => {
     async function loadPosts() {
@@ -20,16 +25,45 @@ export default function Home({ search, tags, page }) {
       });
 
       setPosts(data);
+      setMaxPage(parseInt(headers['x-mc-total-pages'], 10));
     }
 
     loadPosts();
   }, [search, tags, page]);
+
+  async function handlePageChange(index) {
+    const query = {
+      page: index.selected + 1,
+    };
+
+    if (search) query.search = search;
+    if (tags) query.tags = tags;
+
+    await Router.replace({
+      pathname: '/',
+      query,
+    });
+  }
 
   return (
     <Container>
       {posts.map((post, index) => (
         <PostCard key={post.id} post={post} index={index} />
       ))}
+
+      {page && page > 1 && posts.length > 0 ? (
+        <ReactPaginate
+          pageCount={maxPage}
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={3}
+          onPageChange={index => handlePageChange(index)}
+          forcePage={page - 1}
+          containerClassName="pagination-container"
+          activeLinkClassName="active"
+          nextLabel="&#10095;"
+          previousLabel="&#10094;"
+        />
+      ) : null}
     </Container>
   );
 }
